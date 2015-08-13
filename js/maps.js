8a351d6351b2960461f44788f23bfb66384cd0d5
Daniel <danielabrego@gmail.com>
2015-08-13 00:05:31 -0500

    refactored into classes to bettere reflect MVVM pattern

--- js/maps.js
+++ js/maps.js
@@ -1,139 +1,114 @@
-var MapAppObj = function () {
- this.locations = [{
-		"name1" : "South Shore Harbor Resort",
-		"url" : "http://www.sshr.com",
-		"category" : "hotel",
-		"lat" : "29.545310",
-		"lon" : "-95.065914",
-		"idx" : 0,
-		marker : ""
-	}, {
-		"name1" : "Armand Bayou Nature Center",
-		"url" : "http://www.abnc.org",
-		"category" : "nature",
-		"lat" : "29.593904",
-		"lon" : "-95.074970",
-		"idx" : 1,
-		marker : ""
-	}, {
-		"name1" : "Space Center Houston",
-		"url" : "http://www.spacecenter.org",
-		"category" : "entertainment",
-            "lat" : "29.550402",
-		"lon" : "-95.097061",
-		"idx" : 2,
-		marker : ""
-	}, {
-		"name1" : "Kemah Boardwalk",
-		"url" : "http://www.KemahBoardwalk.com",
-		"category" : "entertainment",
-		"lat" : "29.547349",
-		"lon" : "-95.018525",
-		"idx" : 3,
-		marker : ""
-	}, {
-		"name1" : "Tookies Burgers",
-		"url" : "http://www.tookiesburgers.com",
-		"category" : "food",
-		"lat" : "29.563658",
-		"lon" : "-95.025204",
-		"idx" : 4,
-		marker : ""
-	}, {
-		"name1" : "Main Event Entertainment",
-		"url" : "http://www.mainevent.com",
-		"category" : "entertainment",
-		"lat" : "29.524015",
-		"lon" : "-95.122994",
-		"idx" : 5,
-		marker : ""
-	}
-  ];
+/////////////////////////////////////////////////////////////
+//
+// maps.js:
+//
+// This file includes wrapper functions for google maps,
+// and it encapsulates these functions into a single map object
+//
+/////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////
+//
+// MapAppObj():
+//
+// Map object constructor.  Takes an array of location objects
+// as a parameter and stores them for initializeMap().
+//
+//////////////////////////////////////////////////////////////
+var MapAppObj = function (locationsx) {
+  this.locations = locationsx;
+  this.myCenter = new google.maps.LatLng(29.55464378, -95.06847382);
 }  // MapAppObj
 
 
-// create map and markers
+//////////////////////////////////////////////////////////////
+//
+// initializeMap():
+//
+// This function will create a google map, markers, and attach
+// respective click events to the map and markers.
+//
+//////////////////////////////////////////////////////////////
 MapAppObj.prototype.initializeMap = function() {
-    var self=this;
+  var self=this;
 
-    var myCenter = new google.maps.LatLng(29.55464378, -95.06847382);
+  var infowindow = new google.maps.InfoWindow;
 
-    var infowindow = new google.maps.InfoWindow;
+  var mapProp = {
+      center : this.myCenter,
+      zoom : 12,
+      mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
 
-    var mapProp = {
-        center : myCenter,
-        zoom : 12,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  var i;
 
-    var i;
+  // create the map markers for each location
+  for (i = 0; i < self.locations.length; i++) {
+      marker = new google.maps.Marker({
+          animation : google.maps.Animation.DROP,
+          position : new google.maps.LatLng(self.locations[i].lat, self.locations[i].lon),
+          map : map
+      });
 
-    // create the map markers for each location
-    for (i = 0; i < self.locations.length; i++) {
-        marker = new google.maps.Marker({
-                animation : google.maps.Animation.DROP,
-                position : new google.maps.LatLng(self.locations[i].lat, self.locations[i].lon),
-                map : map
-            });
+      // this adds live marker animation when clicked, make it timeout after a few bounces
+      // https://developers.google.com/maps/documentation/javascript/examples/marker-animations
+      google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        return function () {
+          if (marker.getAnimation() != null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
 
-        // this adds live marker animation when clicked, make it timeout after a few bounces
-        // https://developers.google.com/maps/documentation/javascript/examples/marker-animations
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
-                    if (marker.getAnimation() != null) {
-                        marker.setAnimation(null);
-                    } else {
-                        marker.setAnimation(google.maps.Animation.BOUNCE);
+            // don't let marker bounce indefinitely
+            setTimeout(function () {
+                marker.setAnimation(null);
+            }, 2800);
+          }
+        }
+      })(marker, i));
 
-                        // don't let marker bounce indefinitely
-                        setTimeout(function () {
-                            marker.setAnimation(null);
-                        }, 2800);
-                    }
-                }
-            })(marker, i));
+      // adding the click listener for the marker's infowindow
+      // on the marker's infowindow, create a button with a link to a popupwindow
+      google.maps.event.addListener(marker, 'click', (function (marker, i) {
+          return function () {
+            var iws = "";
+            //console.log(self.locations[i].url);
+            //iws += "<button onclick='myPopupFunction(\"" + self.locations[i].url + "\")'> " + self.locations[i].name1 + "</button> " + i;
 
-        // adding the click listener for the marker's infowindow
-        // on the marker's infowindow, create a button with a link to a popupwindow
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
-                    var iws = "";
-                    //console.log(self.locations[i].url);
-                    //iws += "<button onclick='myPopupFunction(\"" + self.locations[i].url + "\")'> " + self.locations[i].name1 + "</button> " + i;
+            clickStr = "window.open('" + self.locations[i].url + "' , '_blank', 'width=400, height=400');";
 
-                    clickStr = "window.open('" + self.locations[i].url + "' , '_blank', 'width=400, height=400');";
+            console.log("clickstr=" + clickStr);
+            iws += "<button onclick=\"" + clickStr + "\"> " + self.locations[i].name1 + "</button> ";
+            console.log(iws);
+            infowindow.setContent(iws);
+            infowindow.open(map, marker);
+          }
+      })(marker, i));
 
-                    console.log("clickstr=" + clickStr);
-                    iws += "<button onclick=\"" + clickStr + "\"> " + self.locations[i].name1 + "</button> " + i;
-                    console.log(iws);
-                    infowindow.setContent(iws);
-                    infowindow.open(map, marker);
-                }
-            })(marker, i));
+      // save markers reference so we can manipulate them later
+      self.locations[i].marker = marker;
+  }
+} // initializeMap()
 
-        // save markers reference so we can manipulate them later
-        self.locations[i].marker = marker;
-    }
-}
-
+//////////////////////////////////////////////////////////////
+//
+// triggerMarker():
+//
+// This function is used to trigger the click event on a single
+// map marker.  It will be bound by knockout framework to the
+// select list in the html page (the list that is produced when a
+// filter is applied).  
+//
+//////////////////////////////////////////////////////////////
 MapAppObj.prototype.triggerMarker = function(idx) {
 	//google.maps.event.trigger(gMarkersArray[idx], 'click');
 	google.maps.event.trigger(this.locations[idx].marker, 'click');
-}
-
-// this function will be called from a link in the marker infowindow
-// it will popup the url passed to it in a small window
-//function myPopupFunction(url1) {
-	//window.open('http://www.w3schools.com', '_blank', 'width=400, height=400');
-//	console.log("popupfunction " + url1);
-	//window.open(url1, '_blank', 'width=400, height=400');
-//};
+} // triggerMarker()
 
 
 
-var mapAppObj = new MapAppObj();
 
-// initialize map on window load event
-google.maps.event.addDomListener(window, 'load', mapAppObj.initializeMap());
---
+++ js/viewmodel.js
@@ -0,0 +1,146 @@
+//////////////////////////////////////////////////////////////
+//
+// viewmodel.js:
+//
+// This file will include the model and viewmodel.
+//
+//////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+//
+// Model():
+//
+// This class is used to create the model for the MVVC pattern.
+// It will be solely location data.
+//
+//////////////////////////////////////////////////////////////
+var Model = function () {
+  this.locations = [{
+         "name1" : "South Shore Harbor Resort",
+         "url" : "http://www.sshr.com",
+         "category" : "hotel",
+         "lat" : "29.545310",
+         "lon" : "-95.065914",
+         "idx" : 0,
+         marker : ""
+     }, {
+         "name1" : "Armand Bayou Nature Center",
+         "url" : "http://www.abnc.org",
+         "category" : "nature",
+         "lat" : "29.593904",
+         "lon" : "-95.074970",
+         "idx" : 1,
+         marker : ""
+     }, {
+         "name1" : "Space Center Houston",
+         "url" : "http://www.spacecenter.org",
+         "category" : "entertainment",
+             "lat" : "29.550402",
+         "lon" : "-95.097061",
+         "idx" : 2,
+         marker : ""
+     }, {
+         "name1" : "Kemah Boardwalk",
+         "url" : "http://www.KemahBoardwalk.com",
+         "category" : "entertainment",
+         "lat" : "29.547349",
+         "lon" : "-95.018525",
+         "idx" : 3,
+         marker : ""
+     }, {
+         "name1" : "Tookies Burgers",
+         "url" : "http://www.tookiesburgers.com",
+         "category" : "food",
+         "lat" : "29.563658",
+         "lon" : "-95.025204",
+         "idx" : 4,
+         marker : ""
+     }, {
+         "name1" : "Main Event Entertainment",
+         "url" : "http://www.mainevent.com",
+         "category" : "entertainment",
+         "lat" : "29.524015",
+         "lon" : "-95.122994",
+         "idx" : 5,
+         marker : ""
+     }
+  ]; // locations
+} // Model()
+
+//////////////////////////////////////////////////////////////
+//
+//  viewModel():
+//
+//  This class will create the view model for the MVVC pattern.
+//  - create google maps object
+//  - create map markers
+//  - apply knockout bindings to html elements to create a filterable list
+//    that shows and hides map markers and list elements based on
+//    values in a text box
+//
+//////////////////////////////////////////////////////////////
+var viewModel = function () {
+  var self = this;
+
+  // store reference to our model
+  var model = new Model();
+
+  // create a map wrapper object to perform operations on our map
+  var mapAppObj = new MapAppObj(model.locations);
+
+  // initialize map on window load event
+  google.maps.event.addDomListener(window, 'load', mapAppObj.initializeMap());
+
+  self.filterText = ko.observable("");
+
+  self.placesArrayObs = ko.observableArray(model.locations);
+
+  // this function will animate the marker that corresponds with the item (from the filter search) that was pressed
+  self.selectMarker = function (item) {
+      console.log(item.idx);
+      mapAppObj.triggerMarker(item.idx);
+  }
+
+  // utility function used to compare filter string to search text
+  // http://stackoverflow.com/questions/17557789/using-contains-instead-of-stringstartswith-knockout-js
+  self.stringStartsWith = function (string, startsWith) {
+    string = string || "";
+    if (startsWith.length > string.length)
+      return false;
+    return string.substring(0, startsWith.length) === startsWith;
+  }
+
+  self.SearchResults1 = ko.computed(function () {
+
+    // first hide all markers (markers stored in map.js)
+    for (i1 = 0; i1 < model.locationslength; i1++) {
+      model.locations[i1].marker.setVisible(false);
+    }
+
+    return ko.utils.arrayFilter(self.placesArrayObs(), function (item) {
+      // match marker here
+      // then set visibility of pin  pin.marker.setVisible(match);
+
+      var filter = self.filterText().toLowerCase();
+
+      // only show markers that have a name that matches the filter text box
+      for (i1 = 0; i1 < model.locations.length; i1++) {
+        if (self.stringStartsWith(model.locations[i1].name1.toLowerCase(), filter)) {
+          model.locations[i1].marker.setVisible(true);
+        }
+      }
+
+      return self.stringStartsWith(item.name1.toLowerCase(), filter);
+    });
+  });
+}; // viewModel
+
+
+//////////////////////////////////////////////////////////////
+//
+// Now apply knockout bindings here
+//
+//////////////////////////////////////////////////////////////
+ko.applyBindings(new viewModel());
+
